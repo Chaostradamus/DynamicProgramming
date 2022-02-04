0b6f1b47095cf6c4ab8f0c65e9b0cdbc4761bcde
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-02-04 02:39:00 -0500

    bst

---
+++ BST/bst.js
@@ -0,0 +1,132 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BST {
+  constructor(value) {
+    this.root = new Node(value);
+    this.count = 1;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  insert(value) {
+    this.count++;
+
+    let newNode = new Node(value);
+    const searchTree = (node) => {
+      if (value < node.value) {
+        if (!node.left) {
+          node.left = newNode;
+        } else {
+          searchTree(node.left);
+        }
+      } else if (value > node.value) {
+        if (!node.right) {
+          node.right = newNode;
+        } else {
+          searchTree(node.right);
+        }
+      }
+    };
+    searchTree(this.root);
+  }
+
+  min() {
+    let current = this.root;
+
+    while (current.left) {
+      current = current.left;
+    }
+
+    return current.value;
+  }
+
+  max() {
+    let current = this.root;
+    while (current.right) {
+      current = current.right;
+    }
+    return current.value;
+  }
+
+  contains(value) {
+    let current = this.root;
+
+    while (current) {
+      if (value === current.value) return true;
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  inOrder() {
+    let result = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      result.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  preOrder() {
+    let result = [];
+    const traverse = (node) => {
+      result.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  postOrder() {
+    let result = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.value);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  bfs() {
+    let result = [];
+    let queue = [];
+
+    queue.push(this.root);
+
+    while (queue.length) {
+      let current = queue.shift();
+      result.push(current.value);
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return result;
+  }
+}
+
+const bst = new BST(6);
+
+bst.insert(3);
+bst.insert(24);
+bst.insert(1987);
+bst.insert(26);
+bst.insert(36);
+bst.insert(16);
+
+console.log(bst.bfs());
